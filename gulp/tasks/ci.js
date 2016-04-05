d9b1aee11c5f5ecabdc56f2fbdd9fb03d96b9463
Gilles TOUBIANA <gilles.toubiana@yahoo.fr>
2016-04-05 14:31:59 +0200

    coveralls tests

--- gulp/tasks/ci.js
+++ gulp/tasks/ci.js
@@ -18,16 +18,16 @@ gulp.task('ci', sequence(
   // Spécifications des tests
   'specifications',
 
+  // Couvertures des tests avec istanbul et COVERALLS
+  'couvertures',
+
   // Certification eslint airbnb/legacy du fichier acte.js
   'validations.script'
 
 ));
 
-// Tâche CI pour node 5 vers des services tiers
-gulp.task('ci.services', sequence(
-
-  // Couvertures des tests avec COVERALLS
-  'couvertures',
+// Tâche CI pour node 5
+gulp.task('services', sequence(
 
   // Tests karma avec SAUCELABS
   'specifications.karma'
