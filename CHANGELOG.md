954c15ac3a8e4fd6e47b9be89c68e0efb434b6ab
Gilles TOUBIANA <gilles.toubiana@yahoo.fr>
2016-04-07 14:42:30 +0200

    feat(gulp): Script pour les releases

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+<a name="0.0.5"></a>
+## [0.0.5](https://github.com/gtoubiana/acte/compare/v0.0.4...v0.0.5) (2016-04-07)
+
+
+
+
--- gulp/tasks/releases.js
+++ gulp/tasks/releases.js
@@ -1,7 +1,11 @@
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/automate-release-workflow.md
-var bump = require('gulp-bump');
-var conventionalChangelog = require('gulp-conventional-changelog');
 var gulp = require('gulp');
+var sequence = require('gulp-sequence');
+var cChangelog = require('gulp-conventional-changelog');
+var cGithubReleaser = require('conventional-github-releaser');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+var fs = require('fs');
 
 gulp.task('releases.prerelease', function () {
   gulp.src('./package.json')
@@ -31,10 +35,84 @@ gulp.task('releases.changelog', function () {
   return gulp.src('CHANGELOG.md', {
     buffer: false
   })
-    .pipe(conventionalChangelog({
+    .pipe(cChangelog({
 
       // Or to any other commit message convention you use.
       preset: 'angular'
     }))
     .pipe(gulp.dest('./'));
 });
+
+gulp.task('releases.github', function (done) {
+  cGithubReleaser({
+    type: 'oauth',
+
+    /* CONVENTIONAL_GITHUB_RELEASER_TOKEN
+     * le fichier karma/sauce.json NE DOIT JAMAIS ÊTRE DIVULGUÉ ! */
+    token: require(__dirname + '/../../test/karma/sauce').cGithubReleaserToken
+  }, {
+
+    // Or to any other commit message convention you use.
+    preset: 'angular'
+  }, done);
+});
+
+gulp.task('releases.commit', function () {
+  return gulp.src('.')
+    .pipe(git.add())
+    .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('releases.push', function (cb) {
+  git.push('origin', 'gh-pages', cb);
+});
+
+gulp.task('releases.tag', function () {
+  var version = function getPackageJsonVersion() {
+    // We parse the json file instead of using require because require caches
+    // multiple calls so the version number won't be updated
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  };
+
+  git.tag(version, 'Created Tag for version: ' + version, function (err) {
+    if (err) throw err;
+  });
+});
+
+// Push with Tag
+gulp.task('releases.pushtag', function (cb) {
+  git.push('origin', 'gh-pages', { args: '--tags' }, cb);
+});
+
+
+// Tâche de la release
+gulp.task('prerelease', sequence(
+
+  // nettoyage des fichiers inutiles
+  'nettoyages',
+
+  // version patch
+  'releases.prerelease',
+
+  // nouveau build
+  'default',
+
+  // Génération du changelog
+  'releases.changelog',
+
+  // git commit
+  'releases.commit',
+
+  // git push
+  'releases.push',
+
+  // tag de la version
+  'releases.tag',
+
+  // git push du tag de la version
+  'releases.pushtag',
+
+  // github release
+  'releases.github'
+
+));
