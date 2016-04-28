44a2adb0c7504e2622e4447d20b467b15fa11728
Gilles TOUBIANA <gilles.toubiana@yahoo.fr>
2016-04-28 13:57:08 +0200

    shore(babel): charge les require()

--- gulp/tasks/depcheck.js
+++ gulp/tasks/depcheck.js
@@ -2,13 +2,28 @@ var depcheck = require('gulp-depcheck');
 var gulp = require('gulp');
 
 // dépendances
-// require('babel-plugin-transform-es2015-ie8-classes');
+require('babel-helper-transform-fixture-test-runner');
+require('babel-plugin-check-es2015-constants');
+require('babel-plugin-transform-es2015-arrow-functions');
+require('babel-plugin-transform-es2015-block-scoped-functions');
+require('babel-plugin-transform-es2015-block-scoping');
 require('babel-plugin-transform-es2015-classes');
-
-// require('babel-plugin-transform-es5-property-mutators');
-// require('babel-plugin-transform-jscript');
-// require('babel-preset-es2015-loose-ie8');
-require('babel-preset-es2015-without-strict');
+require('babel-plugin-transform-es2015-computed-properties');
+require('babel-plugin-transform-es2015-destructuring');
+require('babel-plugin-transform-es2015-duplicate-keys');
+require('babel-plugin-transform-es2015-for-of');
+require('babel-plugin-transform-es2015-function-name');
+require('babel-plugin-transform-es2015-literals');
+require('babel-plugin-transform-es2015-modules-commonjs');
+require('babel-plugin-transform-es2015-object-super');
+require('babel-plugin-transform-es2015-parameters');
+require('babel-plugin-transform-es2015-shorthand-properties');
+require('babel-plugin-transform-es2015-spread');
+require('babel-plugin-transform-es2015-sticky-regex');
+require('babel-plugin-transform-es2015-template-literals');
+require('babel-plugin-transform-es2015-typeof-symbol');
+require('babel-plugin-transform-es2015-unicode-regex');
+require('babel-plugin-transform-regenerator');
 require('eslint-config-airbnb');
 require('eslint-plugin-import');
 require('eslint-plugin-jsx-a11y');
@@ -21,9 +36,6 @@ require('karma-jasmine');
 require('karma-opera-launcher');
 require('karma-safari-launcher');
 require('karma-sauce-launcher');
-require('babel-plugin-transform-es3-member-expression-literals');
-require('babel-plugin-transform-es3-property-literals');
-
 
 // Vérifier les dépendances
 gulp.task('depcheck', depcheck({
--- gulp/tasks/travis.js
+++ gulp/tasks/travis.js
@@ -1,55 +1,10 @@
 /** TACHES PRINCIPALES DU FICHIER :
- * gulp
  * gulp travis
  */
 var gulp = require('gulp');
 var sequence = require('gulp-sequence');
 
-require('eslint-config-airbnb');
-require('eslint-plugin-react');
-require('eslint-plugin-jsx-a11y');
-require('eslint-plugin-import');
-require('karma-jasmine');
-require('karma-chrome-launcher');
-require('karma-firefox-launcher');
-require('karma-opera-launcher');
-require('karma-safari-launcher');
-require('karma-sauce-launcher');
-require('babel-preset-es2015-without-strict');
-
-// Tâche travis ci
+// Tâche build par défaut
 gulp.task('travis', sequence(
-
-  // Nettoyages
-  'clean.dist',
-  'clean.tests',
-
-  // Vérification des dépendances
-  'depcheck',
-
-  // Validations des fichiers src
-  'lint.gulp',
-  'lint.specs',
-  'lint.constants',
-  'lint.functions',
-  'lint.constructors',
-  'lint.prototypes',
-
-  // Spécifications des tests [lint.specs]
-  'tests.specs',
-
-  // Générations des documentations
-  // [lint.constants, lint.functions, lint.constructors, lint.prototypes]
-  'docs.dist',
-  'docs.constants',
-  'docs.functions',
-
-  // Générations des fichiers [docs.dist]
-  'dist.acte',
-  'dist.min',
-  'dist.zip',
-
-  // Validation airbnb es5 [dist.acte]
-  'lint.dist'
-
+  'default'
 ));
