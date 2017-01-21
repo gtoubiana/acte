1ec91208f9068e832126bdabb6970fb5d0a1862b
Gilles TOUBIANA <gilles.toubiana@yahoo.fr>
2017-01-21 14:47:53 +0100

    chore(gulp): Ajout d'un listing des tâches
    `gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 // http://makina-corpus.com/blog/metier/2015/make-your-gulp-modular
+const gulp = require('gulp');
 const requireDir = require('require-dir');
+const taskListing = require('gulp-task-listing');
+
+// Add a task to render the output
+gulp.task('tasks', taskListing);
 
 requireDir('./gulp/tasks', {
   recurse: true,
